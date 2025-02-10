4e2993fd872775885df5594809590a99076082b1
Harsh-kumar-2024 <hkakadiya3102@gmail.com>
2025-02-10 21:15:00 +0530

    k sum paths

---
+++ Trees/KSumPaths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+      int countPathsUtil(Node* node, int k, int currSum, 
+                             unordered_map<int, int>& prefSums) {
+    
+      if (node == nullptr)
+          return 0;
+    
+        int pathCount = 0;
+      currSum += node->data;
+      if (currSum == k)
+          pathCount++;
+      pathCount += prefSums[currSum - k];
+      prefSums[currSum]++;
+      pathCount += countPathsUtil(node->left, k, currSum, prefSums);
+      pathCount += countPathsUtil(node->right, k, currSum, prefSums);
+      prefSums[currSum]--;
+    
+        return pathCount;
+  }
+      int sumK(Node *root, int k) {
+          unordered_map<int, int> prefSums;
+          return countPathsUtil(root, k, 0, prefSums);
+      }
+  };
